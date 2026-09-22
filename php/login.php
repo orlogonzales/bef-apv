e2c1d2214fd07f930c2929306188802e6f5a6130
Orlando Gonzales <orlogonzales@gmail.com>
2026-09-21 23:48:01 -0500

    fase-0.3: corrige compatibilidad php83 del login

--- php/login.php
+++ php/login.php
@@ -1,12 +1,13 @@
 <?php
 	include('funciones.php');
-	$usuario = $_POST[usuario];
-	$clave   = md5($_POST[clave]);
+	$usuario = $_POST['usuario'];
+	$clave   = md5($_POST['clave']);
 	$login   = login($usuario, $clave);
 	$estadoLogin = $login['resultado'];
 	$rol = $login['rol'];
 	$descripcionRol = $login['descripcionRol'];
 	$nombreUsuario = $login['nombreUsuario'];
+	$respuesta = new stdClass();
 	$respuesta->estadoLogin=$estadoLogin;
 	$respuesta->rol=$rol;
 	$respuesta->descripcionRol=$descripcionRol;
