de1e8b8905abc612651ded388479fd97639ba038
Orlando Gonzales <orlogonzales@gmail.com>
2026-09-26 09:39:19 -0500

    fase-0.72: hardening php83 lista-cuentas-banco

--- php/lista-cuentas-banco.php
+++ php/lista-cuentas-banco.php
@@ -6,9 +6,9 @@
 	$rs=mysqli_query($conexion,$sql);
 	echo '<option value="" selected>SELECCIONE CUENTA BANCARIA</option>';
 	while($datos=mysqli_fetch_array($rs)){
-		$codCuenta    =$datos[codigoCuenta];
-		$numeroCuenta =$datos[numeroCuenta];
-		$detalle      =$datos[detalle];
+		$codCuenta    =$datos['codigoCuenta'];
+		$numeroCuenta =$datos['numeroCuenta'];
+		$detalle      =$datos['detalle'];
 		$infoCuenta   =infoBancos($codigoBanco,'detalleEntidad').' - '.texto($detalle).' ('.$numeroCuenta.')';
 		echo '<option value="'.$codCuenta.'">'.$infoCuenta.'</option>';
 	}
