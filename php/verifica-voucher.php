f1f88949904a3d78388ec6d37f7f71dfba99ec19
Orlando Gonzales <orlogonzales@gmail.com>
2026-09-26 09:53:53 -0500

    fase-0.73: hardening php83 verifica-voucher

--- php/verifica-voucher.php
+++ php/verifica-voucher.php
@@ -2,7 +2,7 @@
 	session_start();
 	include ('funciones.php');
 	$conexion        =conexionDB();
-	$nroDocumento    =$_POST[documentoPago];
+	$nroDocumento    =$_POST['documentoPago'];
 	$verificaDOCPago =verificaDOC($nroDocumento,'VOU');
 	if($verificaDOCPago=='EXISTE'){ $respuesta->mensaje ="EXISTE"; }
 	if($verificaDOCPago=='NOEXISTE'){ $respuesta->mensaje ="NOEXISTE"; }
